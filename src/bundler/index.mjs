8b5e4fe75e851a89fa770ee0365fb7f94773dced
mzkmnk <sma0729mex@gmail.com>
2025-04-08 22:37:37 +0900

    feat: import,export

--- src/bundler/index.mjs
+++ src/bundler/index.mjs
@@ -51,19 +51,90 @@ const buildModuleTemplateString = (moduleCode,index) => {
 const buildRuntimeTemplateString = (allModules,indexLocation) => {
   return `
   (function(modules){
+  
     const installedModules = {};
+    
     function _our_require_(moduleId){
       if(installedModules[moduleId]){
         return installedModules[moduleId].exports;
       }
       
       const module = {
-        id: moduleId,
+        i: moduleId,
         exports: {},
       }
     }
+    
+    modules[moduleId].call({},
+      module,
+      _our_require_
+    )
+    
+    const exports = module.exports;
+    
+    installedModules[moduleId] = exports;
+    
+    return _our_require_(${indexLocation});
   })
+  ([${allModules}]);
   `
+}
+
+const getImport = (item, allDeps) => {
+
+  const importFunctionName = item.specifiers[0].imported.name;
+
+  const fileImported = item.value.source;
+
+  const fullFile = path.resolve(fileImported);
+
+  const itemId = allDeps.findIndex(item => item.name === fullFile);
+
+  return {
+    type: "VariableDeclaration",
+    kind: "const",
+    declarations: [
+      {
+        type: "VariableDeclarator",
+        init: {
+          type: "CallExpression",
+          callee: {
+            type: "Identifier",
+            name: "_ourRequire"
+          },
+          arguments: [
+            {
+              type: "Literal",
+              value: itemId
+            }
+          ]
+        },
+        id: {
+          type: "Identifier",
+          name: importFunctionName
+        }
+      }
+    ]
+  }
+}
+
+const getExport = item => {
+  const moduleName = item.specifiers[0].exported.name;
+
+  return {
+    type: "ExpressionStatement",
+    expression: {
+      type: "AssignmentExpression",
+      left: {
+        type: "MemberExpression",
+        object: { type: "Identifier", name: "module" },
+        computed: false,
+        property: { type: "Identifier", name: "exports" }
+      },
+      operator: "=",
+      right: { type: "Identifier", name: moduleName }
+    }
+  }
 }
 
 const targetPath = path.resolve(__dirname,'./main.mjs');
